1275de1922b054ce7adaefe82abdfe882449d670
Fatima <fatima.moqran@gmail.com>
2020-10-19 17:38:51 +0200

    add database

--- flask_app.py
+++ flask_app.py
@@ -16,13 +16,25 @@ class User(db.Model):
     prenom = db.Column(db.String, nullable=False)
     login = db.Column(db.String(20), unique= True, nullable= False)
     mot_de_passe = db.Column(db.String(60), nullable = False)
+    #spécifier la relation entre les deux classes
+    email= db.relationship('Email', backref='sender', lazy=True)
 
     #comment l'objet sera printé 
     def __repr__(self):
-        return f"User('{self.nom}','{self.prenom}','{self.login}')"
+        return f"User('{self.nom}','{self.prenom}','{self.login}')"#on ne montre pas le mot de passe quand on voudra le représenter
 
 class Email(db.Model):
     id = db.Column(db.Integer,primary_key=True)
+    texte = db.Column(db.Text, nullable=False) 
+    categorie_model = db.Column(db.String(100), nullable=False)
+    categorie_choisie= db.Column(db.String(100), nullable=False)
+    #Foreign key 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+
+     #comment l'objet sera printé 
+    def __repr__(self):
+        return f"Email('{self.categorie_model}','{self.categorie_choisie}')"
+
     
 
 @app.route('/')
@@ -35,5 +47,5 @@ def admin():
     return render_template('administrateur.html')
 
 #permet de runner l'application directement dans python
-if __name__ == __name__:
+if __name__ == '__main__':
     app.run(debug=True)
